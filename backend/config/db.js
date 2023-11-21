764e75006fc57e7ff12426647c580fe9f2170862
Himanshu Tripathi <123529078+Himanshuadv@users.noreply.github.com>
2023-11-21 17:24:37 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,12 +5,12 @@ const connectDb = async () => {
     let conn;
 
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/taskify", { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect("mongodb://127.0.0.1:27017/taskify");
         conn = mongoose.connection;
         console.log(`MongoDB Connected: ${conn.host}`);
     } catch (error) {
         console.log(`Error: ${error.message}`);
-        process.exit();
+        process.exit(1);
     }
 };
 
